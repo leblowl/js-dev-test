cd1bf612c042b8199384bb59925a4cdc133d14d4
Lucas Leblow <leblowl@gmail.com>
2019-09-17 19:26:10 -0600

    Add button bar to UI and allow user to filter repo list by language; add view-model for repo list to bridge between the list of repo entities and the displayed repo list entities

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Repo, AppState} from './App.typings';
+import { AppState, Repo, RepoListVM} from './App.typings';
 
 // Util
 
@@ -17,18 +17,57 @@ const get = function(url: string, onload: Function) {
   req.send();
 }
 
+function onlyUnique(value: any, index: number, self: Array<any>) {
+      return self.indexOf(value) === index;
+}
+
 // Events
 
-const onSelectRepo = function(app: React.Component<Object, AppState>, id: number) {
-  app.setState({
-    repos: app.state.repos.map((x) => {
+const onInitState = function(
+  app: React.Component<Object, AppState>,
+  data: Array<Repo>
+) {
+  app.setState((state, props) => {
+    state.entities.repos = data;
+    state.repoList.repos = state.entities.repos;
+    state.repoList.languages = [ANY_LANGUAGE].concat(state.entities.repos.map(
+      (x) => x.language).filter(onlyUnique));
+    return state;
+  });
+}
+
+const onSelectRepo = function(
+  app: React.Component<Object, AppState>,
+  id: number
+) {
+  app.setState((state, props) => {
+    state.repoList.repos = state.entities.repos.map((x) => {
       if (x.id === id) {
         x.selected = !x.selected;
         return x;
       } else {
         return x;
-      }})
- });
+      }});
+    return state;
+  });
+}
+
+const ANY_LANGUAGE = 'Any';
+
+const onSortByLanguage = function(
+  app: React.Component<Object, AppState>,
+  language: string
+) {
+  app.setState((state, props) => {
+    if (language === ANY_LANGUAGE) {
+      state.repoList.repos = state.entities.repos;
+    } else {
+      state.repoList.repos = state.entities.repos.filter((x) => {
+        return x.language === language;
+      })
+    }
+    return state;
+  });
 }
 
 // Views
@@ -52,30 +91,60 @@ const RepoItem: React.FunctionComponent<{
   );
 };
 
-const RepoList: React.FunctionComponent<{
-  repos: Array<Repo>;
+const LanguageButtons: React.FunctionComponent<{
+  languages: Array<string>;
+  languageSelected: string;
   emit: Function;
-}> = function( { repos, emit }) {
+}> = function({ languages, languageSelected, emit }) {
+
+
   return (
-    <ul className='repo-list'>
-      {repos.map((x) => {
-        return <RepoItem key={x.id} repo={x} emit={emit} />
+    <div className='button-row'>
+      {languages.map((x) => {
+        return <button key={x} onClick={() => emit(onSortByLanguage, x)}>{x}</button>;
       })}
-    </ul>
+    </div>
+  )
+}
+
+const RepoList: React.FunctionComponent<{
+  repoList: RepoListVM;
+  emit: Function;
+}> = function( { repoList, emit }) {
+  return (
+    <div className='repo-list'>
+      <LanguageButtons
+        languages={repoList.languages}
+        languageSelected={repoList.languageSelected}
+        emit={emit} />
+
+      <ul className='repo-list'>
+        {repoList.repos.map((x) => {
+          return <RepoItem key={x.id} repo={x} emit={emit} />
+        })}
+      </ul>
+    </div>
   );
 }
 
 class App extends React.Component<Object, AppState> {
   constructor(props: Object) {
     super(props);
-    this.state = {repos: []};
+    this.state = {
+      entities: {
+        repos: []
+      },
+      repoList: {
+        languages: [],
+        languageSelected: '',
+        repos: []
+      }
+    };
   }
 
   componentDidMount() {
     get('http://localhost:4000/repos', (data: Array<Repo>) => {
-      this.setState({
-        'repos': data
-      });
+      this.emit(this, onInitState, data);
     });
   }
 
@@ -84,10 +153,13 @@ class App extends React.Component<Object, AppState> {
   }
 
   render() {
+    let emit = (event: Function, data: Object) => this.emit(this, event, data);
+
     return (
       <div className='App'>
-        <RepoList repos={this.state.repos}
-                  emit={(event: Function, data: Object) => this.emit(this, event, data)}/>
+        <RepoList
+          repoList={this.state.repoList}
+          emit={emit} />
       </div>
     );
   }
--- web/src/App.typings.tsx
+++ web/src/App.typings.tsx
@@ -7,7 +7,18 @@ export interface Repo extends Object {
   forks_count: number;
 }
 
-export interface AppState extends Object {
+export interface Entities extends Object {
   repos: Array<Repo>;
+}
+
+export interface RepoListVM extends Object {
+  languages: Array<string>;
+  languageSelected: string;
+  repos: Array<Repo>;
+}
+
+export interface AppState extends Object {
+  entities: Entities;
+  repoList: RepoListVM;
 }
 
